58b907f662e1c661a44ac56dad2524ab189e18b1
mahongquan <mahongquan@sina.com>
2014-02-15 10:11:09 +0800

    Update get.py

--- get.py
+++ get.py
@@ -17,22 +17,41 @@ def savefile(pathf):
 
 # res=requests.get("https://raw.github.com/mahongquan/OpenBird/master/Classes/AppDelegate.h")
 # open("AppDelegate.h","w").write(res.content)
-def main():
-    res=requests.get("https://github.com/mahongquan/OpenBird/tree/master/proj.ios_mac/mac")
+def getpath(path):
+    res=requests.get("https://github.com/mahongquan/OpenBird/tree/master/"+path)
     #open("content.html","w").write(res.content)
     soup = BeautifulSoup(res.content)
     rs=soup.tbody.find_all('tr')
     fs=[]
+    paths=[]
     for r in rs:
         cs=r.find_all('td')
-        fs.append(cs[1].a['href'])
+        print cs[0].span['class']
+        if cs[0].span['class'][1]==u"octicon-file-directory":
+            print "ispath"
+            f=cs[1].a['href']
+            ps=f.split("/")
+            childpath="/".join(ps[5:])
+            print childpath
+            paths.append(childpath)
+        else:
+            print "is file"
+            fs.append(cs[1].a['href'])
     for f in fs:
         print f
         ps=f.split("/")
         savefile("/".join(ps[5:]))
+    for p in paths:
+        getpath(p)
+def main():
+    getpath("proj.android/src")
 #savefile("Resources/res/sfx_point.ogg")
 #savefile("proj.ios_mac/FlappyBird.xcodeproj/mac/")
 main()
+
+
+
+
 
 
 
